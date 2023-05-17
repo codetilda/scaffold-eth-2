94ecfd8882138a67af1c8d554868cdfd4476088a
Code Tilda <codetilda@gmail.com>
2023-05-17 18:19:00 +0200

    add type declaration

--- packages/nextjs/pages/nft-minter.tsx
+++ packages/nextjs/pages/nft-minter.tsx
@@ -1,17 +1,25 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-//import Head from "next/head";
+import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/NftMinter.module.css";
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
+type Prediction = {
+    id: string;
+    status: string;
+    output: string[];
+    detail: string;
+};
+
 const NftMinter: NextPage = () => {
-  const [prediction, setPrediction] = useState(null);
+  const [prediction, setPrediction] = useState<Prediction | null>(null);
   const [error, setError] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault(); 
+
     const response = await fetch("/api/predictions", {
       method: "POST",
       headers: {
@@ -45,31 +53,39 @@ const NftMinter: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <>
+        <Head>
+            <title>Mint AI NFT Example UI</title>
+            <meta name="description" content="Created with 🏗 scaffold-eth-2 and Replicate" />
+            {/* We are importing the font this way to lighten the size of SE2. */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+        </Head>
+        <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit} >
+            <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
+            <button type="submit">Go!</button>
+        </form>
 
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
-        <button type="submit">Go!</button>
-      </form>
+        {error && <div>{error}</div>}
 
-      {error && <div>{error}</div>}
-
-      {prediction && (
-        <div>
-            {prediction.output && (
-              <div className={styles.imageWrapper}>
-              <Image
-                fill
-                src={prediction.output[prediction.output.length - 1]}
-                alt="output"
-                sizes='100vw'
-              />
-              </div>
-            )}
-            <p>status: {prediction.status}</p>
+        {prediction && (
+            <div>
+                {prediction.output && (
+                <div className={styles.imageWrapper}>
+                <Image
+                    fill
+                    src={prediction.output[prediction.output.length - 1]}
+                    alt="output"
+                    sizes='100vw'
+                />
+                </div>
+                )}
+                <p>status: {prediction.status}</p>
+            </div>
+        )}
         </div>
-      )}
-    </div>
+    </>
   );
 };
 
